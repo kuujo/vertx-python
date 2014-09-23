15ad3695bfb8bf316d8b13f009730985ea3bfbba
Jordan Halterman <jordan.halterman@gmail.com>
2014-09-22 20:14:11 -0700

    Add basic Python code generation template.

---
+++ src/main/java/io/vertx/lang/python/Gen.java
@@ -0,0 +1,29 @@
+package io.vertx.lang.python;
+
+import io.vertx.codegen.Generator;
+import io.vertx.codegen.Helper;
+
+/**
+ * Actually run the generation
+ *
+ * TODO - we should wrap this in a Maven MOJO so it can run as part of the build lifecyle
+ *
+ * @author <a href="http://tfox.org">Tim Fox</a>
+ */
+public class Gen {
+
+  public static void main(String[] args) {
+    try {
+      new Gen().run();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  public void run() throws Exception {
+    Generator gen = new Generator();
+    gen.genAndApply("io.vertx.core", packageName -> !packageName.contains("impl"),
+      clazz -> "src/main/resources/vertx/" +  Helper.convertCamelCaseToUnderscores(clazz.getSimpleName()) + ".py",
+      "vertx-python/template/python.templ");
+  }
+}
